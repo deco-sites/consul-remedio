98513f8fa790dfa73b2e1b8a7d043808d302b5a9
Enilson Silva <enilson.silva@wicomm.com.br>
2025-02-28 17:08:12 -0300

    Publish 5 from localhost into origin/main

--- apps/deco/ai-assistants.ts
+++
@@ -1,2 +0,0 @@
-export { default } from "apps/ai-assistants/mod.ts";
-export * from "apps/ai-assistants/mod.ts";
--- apps/deco/implementation.ts
+++
@@ -1,2 +0,0 @@
-export { default } from "apps/implementation/mod.ts";
-export * from "apps/implementation/mod.ts";
